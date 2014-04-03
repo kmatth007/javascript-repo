ac5763b06e6dac94beef5f53bd168b87c4a2c944
Kevin E. Matthews <kmatth007@yahoo.com>
2014-04-03 10:07:04 -0400

    delegates

---
+++ Delegates.js
@@ -0,0 +1,13 @@
+function createDelegate(func, target) {
+	return function() {
+		return func.apply(target, arguments);
+	};
+}
+
+var myObject = {name: "Target"};
+function myMethod() {
+	return this.name;
+}
+
+var delegate = createDelegate(myMethod, myObject);
+console.log(delegate());
